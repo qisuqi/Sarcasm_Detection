f1cf66c48de595001703bf9c054f402722ec8f45
QiqiSu <63663984+qisuqi@users.noreply.github.com>
2020-09-25 19:20:54 +0100

    Update Models.py

--- preprocessing/Models.py
+++ preprocessing/Models.py
@@ -1,11 +1,10 @@
-from keras.models import Sequential, Model
-from keras.layers import Dense, LSTM, Embedding, Conv1D, MaxPool1D, Dropout, Flatten, Bidirectional, ReLU, Input
-from keras.layers.merge import concatenate
-import keras.backend as K
-from keras.layers import Layer
+from tensorflow.keras.models import Model
+import tensorflow.keras.backend as K
+from tensorflow.keras.layers import Layer
 import tensorflow as tf
-from tensorflow.keras import layers
-from keras.layers import Permute, Activation
+from tensorflow.keras import layers, regularizers
+from tensorflow.keras.layers import Permute, Activation
+from tensorflow.keras.callbacks import Callback
 
 
 class Attention(Layer):
@@ -64,7 +63,7 @@ class SelfAttention(Layer):
         attention_weights = tf.matmul(W2, attention_score)
         attention_weights = Activation('softmax')(attention_weights)
         embedding_matrix = tf.matmul(attention_weights, inputs)
-        #embedding_matrix_flattened = Flatten()(embedding_matrix)
+        #embedding_matrix_flattened = layers.Flatten()(embedding_matrix)
 
         if self.use_penalise:
             attention_weights_transposed = Permute(dims=(2, 1))(attention_weights)
@@ -74,126 +73,308 @@ class SelfAttention(Layer):
             self.add_loss(self.penalty_coeff * frobenius_norm)
 
         return embedding_matrix
+    
 
+class MultiHeadSelfAttention(Layer):
+    def __init__(self, embed_dim, num_heads=8, maxlen=10):
+        super(MultiHeadSelfAttention, self).__init__()
+        self.embed_dim = embed_dim
+        self.num_heads = num_heads
+        self.maxlen = maxlen
+        if embed_dim % num_heads != 0:
+            raise ValueError(f"embedding dimension = {embed_dim} should be divisible by number of heads = {num_heads}")
 
-def att_BLTSM_CNN(vocab_size=10, dimension=10, embedding_matrix=10, max_length=18):
-    model = Sequential()
-    model.add(Embedding(vocab_size, dimension, weights=[embedding_matrix], input_length=max_length))
-    model.add(Attention())
-    model.add(Bidirectional(LSTM(9, return_sequences=True, dropout=0.2, recurrent_dropout=0.1)))
-    model.add(Conv1D(128, 3, kernel_initializer='normal'))
-    model.add(ReLU())
-    model.add(Conv1D(128, 4, padding='same', kernel_initializer='normal'))
-    model.add(ReLU())
-    model.add(Conv1D(128, 5, padding='same', kernel_initializer='normal'))
-    model.add(ReLU())
-    model.add(MaxPool1D(pool_size=2, strides=2, padding='valid'))
-    model.add(Dropout(0.5))
-    model.add(Flatten())
-    model.add(Dense(1, activation='sigmoid'))
-    return model
+        self.projection_dim = embed_dim // num_heads
+        self.query_dense = layers.Dense(embed_dim)
+        self.key_dense = layers.Dense(embed_dim)
+        self.value_dense = layers.Dense(embed_dim)
+        self.combine_heads = layers.Dense(embed_dim)
 
+    def attention(self, query, key, value):
+        score = tf.matmul(query, key, transpose_b=True)
+        dim_key = tf.cast(tf.shape(key)[-1], tf.float32)
+        scaled_score = score / tf.math.sqrt(dim_key)
+        weights = tf.nn.softmax(scaled_score, axis=-1)
+        output = tf.matmul(weights, value)
+        return output, weights
 
-class BERT(tf.keras.Model):
+    def separate_heads(self, x, batch_size):
+        x = tf.reshape(x, (batch_size, self.maxlen, self.num_heads, self.projection_dim))
+        return tf.transpose(x, perm=[0, 2, 1, 3])
 
-    def __init__(self, vocab_size, embedding_dim=128, cnn_filters=100, dnn_units=512,
-                 ltsm_unit=64, dropout_rate=0.1, training=False, name='sarcasm_model'):
-
-        super(BERT, self).__init__(name=name)
-
-        self.embedding = layers.Embedding(vocab_size, embedding_dim)
-        self.bltsm = layers.Bidirectional(layers.LSTM(ltsm_unit, dropout=dropout_rate, return_sequences=True))
-        self.attention = SelfAttention(size=128, num_hops=10, use_penalise=False)
-        self.cnn1 = layers.Conv1D(filters=cnn_filters, kernel_size=3, padding='valid', activation='relu')
-        self.cnn2 = layers.Conv1D(filters=cnn_filters, kernel_size=4, padding='valid', activation='relu')
-        self.cnn3 = layers.Conv1D(filters=cnn_filters, kernel_size=5, padding='valid', activation='relu')
-        self.pool = layers.GlobalMaxPooling1D()
-        self.dense1 = layers.Dense(units=dnn_units, activation='relu')
-        self.dropout = layers.Dropout(rate=dropout_rate)
-        self.dense2 = layers.Dense(units=1, activation='sigmoid')
-
-    def call(self, inputs, training):
-        l = self.embedding(inputs)
-        l_1 = self.bltsm(l)
-        l_1 = self.attention(l_1)
-        l_1 = self.cnn1(l_1)
-        l_1 = self.pool(l_1)
-        l_2 = self.cnn2(l)
-        l_2 = self.pool(l_2)
-        l_3 = self.cnn3(l)
-        l_3 = self.pool(l_3)
-
-        concat = tf.concat([l_1, l_2, l_3], axis=-1)
-        concat = self.dense1(concat)
-        concat = self.dropout(concat, training)
-        output = self.dense2(concat)
-
+    def call(self, inputs):
+        batch_size = tf.shape(inputs)[0]
+        query = self.query_dense(inputs)
+        key = self.key_dense(inputs)
+        value = self.value_dense(inputs)
+        query = self.separate_heads(query, batch_size)
+        key = self.separate_heads(key, batch_size)
+        value = self.separate_heads(value, batch_size)
+        attention, weights = self.attention(query, key, value)
+        attention = tf.transpose(attention, perm=[0, 2, 1, 3])
+        concat_attention = tf.reshape(attention, (batch_size, self.maxlen, self.embed_dim))
+        output = self.combine_heads(concat_attention)
         return output
 
 
-def satt_BLTSM_CNN(max_length=10, vocab_size=10, dimension=10, embedding_matrix=10):
+def CNN(vocab_size=10, dimension=10, max_length=10, initial_bias=None, embedding_matrix=None):
 
-    inputs = layers.Input(shape=(max_length, ))
-    embedding = layers.Embedding(vocab_size, dimension, weights=[embedding_matrix])(inputs)
-    bltsm = layers.Bidirectional(layers.LSTM(9, return_sequences=True, dropout=0.2, recurrent_dropout=0.1))(embedding)
-    att = SelfAttention(size=128, num_hops=10, use_penalise=False)(bltsm)
+    inputs = layers.Input(shape=(max_length,))
+    embedding = layers.Embedding(vocab_size,
+                                 dimension,
+                                 weights=[embedding_matrix],
+                                 trainable=False)(inputs)
+    x1 = layers.Conv1D(128, 3,
+                       padding='valid',
+                       kernel_initializer='glorot_normal',
+                       activation='relu')(embedding)
+    x1 = layers.GlobalMaxPooling1D()(x1)
+    x2 = layers.Conv1D(128, 3,
+                       padding='valid',
+                       kernel_initializer='glorot_normal',
+                       activation='relu')(embedding)
+    x2 = layers.GlobalMaxPooling1D()(x2)
+    x3 = layers.Conv1D(128, 3,
+                       padding='valid',
+                       kernel_initializer='glorot_normal',
+                       activation='relu')(embedding)
+    x3 = layers.GlobalMaxPooling1D()(x3)
 
-    conv1 = layers.Conv1D(128, 3, kernel_initializer='normal')(att)
-    relu1 = layers.ReLU()(conv1)
-    conv2 = layers.Conv1D(128, 4, padding='same', kernel_initializer='normal')(relu1)
-    relu2 = layers.ReLU()(conv2)
-    conv3 = layers.Conv1D(128, 5, padding='same', kernel_initializer='normal')(relu2)
-    relu3 = layers.ReLU()(conv3)
-    maxpool1 = layers.MaxPool1D(pool_size=2, strides=2, padding='valid')(relu3)
-    drop = layers.Dropout(0.5)(maxpool1)
-    flat1 = layers.Flatten()(drop)
+    merged1 = tf.concat([x1, x2], axis=1)
+    drop1 = layers.Dropout(0.5)(merged1)
 
-    outputs = layers.Dense(1, activation='sigmoid')(flat1)
+    dense = layers.Dense(64, activation='relu')(drop1)
+    drop2 = layers.Dropout(0.36)(dense)
+
+    outputs = layers.Dense(2,
+                          activation='softmax',
+                          bias_initializer=initial_bias)(drop2)
 
     model = tf.keras.Model(inputs=inputs, outputs=outputs)
 
     return model
 
 
-def CNN(vocab_size=10, dimension=10, embedding_matrix=10, max_length=10):
-    model = Sequential()
-    model.add(Embedding(vocab_size, dimension, weights=[embedding_matrix], input_length=max_length))
-    model.add(Conv1D(128, 3, kernel_initializer='uniform'))
-    model.add(ReLU())
-    model.add(Conv1D(128, 4, padding='same', kernel_initializer='uniform'))
-    model.add(ReLU())
-    model.add(Conv1D(128, 5, padding='same', kernel_initializer='uniform'))
-    model.add(ReLU())
-    model.add(MaxPool1D(pool_size=2, strides=2, padding='valid'))
-    model.add(Dropout(0.5))
-    model.add(Flatten())
-    model.add(Dense(1, activation='sigmoid'))
+def BLSTM(embedding_model='glove', vocab_size=10, dimension=10, max_length=10, embedding_matrix=10):
+
+    inputs = layers.Input(shape=(max_length, ))
+
+    if embedding_model == 'glove' or embedding_model == 'word2vec':
+        embedding = layers.Embedding(vocab_size, dimension, weights=[embedding_matrix])(inputs)
+
+    elif embedding_model == 'bert':
+        embedding = layers.Embedding(vocab_size,
+                                     dimension,
+                                     trainable=False,
+                                     embeddings_initializer='uniform')(inputs)
+
+    blstm = layers.Bidirectional(layers.LSTM(200,
+                                             return_sequences=True,
+                                             activation='tanh',
+                                             recurrent_activation='sigmoid',
+                                             dropout=0.44,
+                                             recurrent_dropout=0.14,
+                                             kernel_initializer='glorot_normal',
+                                             recurrent_initializer='he_uniform'))(embedding)
+
+    drop = layers.Dropout(0.4)(blstm)
+
+    flat = layers.Flatten()(drop)
+
+    dense = layers.Dense(64, activation='relu')(flat)
+    drop2 = layers.Dropout(0.36)(dense)
+
+    outputs = layers.Dense(2,
+                           activation='softmax')(drop2)
+
+    model = tf.keras.Model(inputs=inputs, outputs=outputs)
 
     return model
 
-def af_att_BLTSM_CNN(max_length=10, vocab_size=10, dimension=10, embedding_matrix=10):
 
-    inputs1 = Input(shape=(max_length,))
-    embedding = Embedding(vocab_size, dimension, weights=[embedding_matrix])(inputs1)
-    bltsm = Bidirectional(LSTM(9, return_sequences=True, dropout=0.2, recurrent_dropout=0.1))(embedding)
-    att = Attention()(bltsm)
+def att_BLSTM_CNN(att_type='soft_att', embedding_model='glove', vocab_size=10, dimension=10, max_length=10,
+                  embedding_matrix=None, initial_bias=None):
 
-    inputs2 = Input(shape=(max_length, 1))
+    inputs = layers.Input(shape=(max_length,))
 
-    merged = concatenate([att, inputs2])
+    if embedding_model == 'glove' or embedding_model == 'word2vec':
+        embedding = layers.Embedding(vocab_size, dimension, weights=[embedding_matrix])(inputs)
 
-    conv1 = Conv1D(128, 3, kernel_initializer='normal')(merged)
-    relu1 = ReLU()(conv1)
-    conv2 = Conv1D(128, 4, padding='same', kernel_initializer='normal')(relu1)
-    relu2 = ReLU()(conv2)
-    conv3 = Conv1D(128, 5, padding='same', kernel_initializer='normal')(relu2)
-    relu3 = ReLU()(conv3)
-    maxpool1 = MaxPool1D(pool_size=2, strides=2, padding='valid')(relu3)
-    drop = Dropout(0.5)(maxpool1)
-    flat1 = Flatten()(drop)
+    elif embedding_model == 'bert':
+        embedding = layers.Embedding(vocab_size,
+                                     dimension,
+                                     trainable=False,
+                                     embeddings_initializer='uniform')(inputs)
 
-    outputs = Dense(1, activation='sigmoid')(flat1)
+    blstm = layers.Bidirectional(layers.LSTM(200,
+                                             return_sequences=True,
+                                             activation='tanh',
+                                             recurrent_activation='sigmoid',
+                                             dropout=0.44,
+                                             recurrent_dropout=0.14,
+                                             kernel_initializer='glorot_normal',
+                                             recurrent_initializer='he_uniform'))(embedding)
+    drop = layers.Dropout(0.4)(blstm)
 
-    model = Model(inputs=[inputs1, inputs2], outputs=[outputs])
+    if att_type == 'soft_att':
+        att = Attention()(drop)
+
+    elif att_type == 'self_att':
+        att = SelfAttention(size=64, num_hops=20, use_penalise=False)(drop)
+
+    elif att_type == 'multihead_att':
+        att = MultiHeadSelfAttention(dimension, num_heads=5, maxlen=max_length)(drop)
+
+    x1 = layers.Conv1D(128, 3,
+                       padding='valid',
+                       kernel_initializer='glorot_normal',
+                       activation='relu')(att)
+    x1 = layers.GlobalMaxPooling1D()(x1)
+    x2 = layers.Conv1D(128, 3,
+                       padding='valid',
+                       kernel_initializer='glorot_normal',
+                       activation='relu')(att)
+    x2 = layers.GlobalMaxPooling1D()(x2)
+
+    merged1 = tf.concat([x1, x2], axis=1)
+    drop1 = layers.Dropout(0.5)(merged1)
+
+    dense = layers.Dense(200, activation='relu')(drop1)
+    drop2 = layers.Dropout(0.36)(dense)
+
+    outputs = layers.Dense(2,
+                          activation='softmax',
+                          bias_initializer=initial_bias)(drop2)
+
+    model = tf.keras.Model(inputs=inputs, outputs=outputs)
 
     return model
+
+
+def af_att_BLSTM_CNN(att_type='soft_att', embedding_model='glove', vocab_size=10, dimension=10, max_length=10,
+                     embedding_matrix=10, initial_bias=None):
+
+    inputs = layers.Input(shape=(max_length,))
+
+    if embedding_model == 'glove' or embedding_model == 'word2vec':
+        embedding = layers.Embedding(vocab_size,
+                                     dimension,
+                                     weights=[embedding_matrix],
+                                     embeddings_initializer='uniform',
+                                     trainable=False)(inputs)
+
+    elif embedding_model == 'bert':
+        embedding = layers.Embedding(vocab_size,
+                                     dimension,
+                                     embeddings_initializer='uniform',
+                                     input_length=max_length,
+                                     trainable=False)(inputs)
+
+    blstm = layers.Bidirectional(layers.LSTM(200,
+                                             return_sequences=True,
+                                             activation='tanh',
+                                             recurrent_activation='sigmoid',
+                                             dropout=0.44,
+                                             recurrent_dropout=0.14,
+                                             kernel_initializer='glorot_normal',
+                                             recurrent_initializer='he_uniform',
+                                             name='blstm'))(embedding)
+    drop = layers.Dropout(0.4)(blstm)
+
+    if att_type == 'soft_att':
+        att = Attention()(drop)
+
+    elif att_type == 'self_att':
+        att = SelfAttention(size=64, num_hops=20, use_penalise=False)(drop)
+
+    elif att_type == 'multihead_att':
+        att = MultiHeadSelfAttention(dimension, num_heads=5, maxlen=max_length)(drop)
+
+    inputs2 = layers.Input(shape=(20, 1))
+
+    merged = tf.concat([att, inputs2], axis=-1)
+
+    x1 = layers.Conv1D(128, 3,
+                       padding='valid',
+                       kernel_initializer='glorot_normal',
+                       activation='relu')(merged)
+    x1 = layers.GlobalMaxPooling1D()(x1)
+    x2 = layers.Conv1D(128, 3,
+                       padding='valid',
+                       kernel_initializer='glorot_normal',
+                       activation='relu')(merged)
+    x2 = layers.GlobalMaxPooling1D()(x2)
+
+    merged1 = tf.concat([x1, x2], axis=1)
+    drop1 = layers.Dropout(0.5)(merged1)
+
+    dense = layers.Dense(200, activation='relu')(drop1)
+    drop2 = layers.Dropout(0.36)(dense)
+
+    dense2 = layers.Dense(2,
+                          activation='softmax',
+                          bias_initializer=initial_bias)(drop2)
+
+    model = Model(inputs=[inputs, inputs2], outputs=dense2)
+
+    return model
+
+
+def get_masks(tokens, max_seq_length):
+
+    if len(tokens) > max_seq_length:
+        # Cutting down the excess length
+        tokens = tokens[0: max_seq_length]
+        return [1] * len(tokens)
+    else:
+        return [1] * len(tokens) + [0] * (max_seq_length - len(tokens))
+
+
+def get_segments(tokens, max_seq_length):
+    if len(tokens)>max_seq_length:
+      #Cutting down the excess length
+        tokens = tokens[:max_seq_length]
+        segments = []
+        current_segment_id = 0
+        for token in tokens:
+            segments.append(current_segment_id)
+            if token == "[SEP]":
+                current_segment_id = 1
+        return segments
+    else:
+        segments = []
+        current_segment_id = 0
+        for token in tokens:
+            segments.append(current_segment_id)
+            if token == "[SEP]":
+                current_segment_id = 1
+        return segments + [0] * (max_seq_length - len(tokens))
+
+
+def get_ids(tokens, tokenizer, max_seq_length):
+
+    if len(tokens)>max_seq_length:
+        tokens = tokens[:max_seq_length]
+        token_ids = tokenizer.convert_tokens_to_ids(tokens)
+        return token_ids
+    else:
+        token_ids = tokenizer.convert_tokens_to_ids(tokens)
+        input_ids = token_ids + [0] * (max_seq_length-len(token_ids))
+        return input_ids
+
+
+class TerminateOnBaseline(Callback):
+    """Callback that terminates training when either acc or val_acc reaches a specified baseline
+    """
+    def __init__(self, monitor='val_accuracy', baseline=0.9):
+        super(TerminateOnBaseline, self).__init__()
+        self.monitor = monitor
+        self.baseline = baseline
+
+    def on_epoch_end(self, epoch, logs=None):
+        logs = logs or {}
+        acc = logs.get(self.monitor)
+        if acc is not None:
+            if acc >= self.baseline:
+                print('Epoch %d: Reached baseline, terminating training' % (epoch))
+                self.model.stop_training = True
